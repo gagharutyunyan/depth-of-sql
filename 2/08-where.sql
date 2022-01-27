983bb6b2b1e739f541880d0f5920d5b0269dc8dd
gagharutyunyan <gagharut@gmail.com>
2022-01-27 19:18:16 +0300

    between

--- 2/08-where.sql
+++ 2/08-where.sql
@@ -1,2 +1,8 @@
+-- SELECT * FROM sales 
+-- WHERE id > 8  AND id < 10;
+
+-- SELECT * FROM sales 
+-- WHERE is_recurring = TRUE;
+
 SELECT * FROM sales 
-WHERE id > 8  AND id < 10;
+WHERE is_recurring = TRUE AND volume < 5;
---
+++ 2/09-date-range.sql
@@ -0,0 +1,8 @@
+-- SELECT * FROM sales 
+-- WHERE date_created > '2022-01-17' AND date_created < '2022-01-20';
+
+-- SELECT * FROM sales 
+-- WHERE date_created BETWEEN '2022-01-17' AND '2022-01-20';
+
+SELECT * FROM sales 
+WHERE volume BETWEEN 1099.819 and 2000;
