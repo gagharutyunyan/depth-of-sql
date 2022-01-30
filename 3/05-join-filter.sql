3ee6d6d1b24a4a4423b0522b241c099d133c9704
gagharutyunyan <gagharut@gmail.com>
2022-01-30 13:23:12 +0300

    union and cross join

--- 3/05-join-filter.sql
+++ 3/05-join-filter.sql
@@ -4,3 +4,5 @@ INNER JOIN addresses AS a ON u.address_id = a.id
 INNER JOIN cities AS c ON a.city_id = c.id
 WHERE c.name = 'Lipetsk'
 ORDER BY full_name DESC;
+
+-- INNER JOIN cities AS c ON a.city_id = c.id -- left join это когда все таблицы мерджатся с крайней левой таблицей, те значения, которые пустые становятся Null
---
+++ 3/07-union.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users
+WHERE id < 2
+UNION
+SELECT * FROM users
+WHERE id > 4;
