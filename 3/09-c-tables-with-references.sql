f9756a59669c7ed956f035b5fff6fdc04f66da46
gagharutyunyan <gagharut@gmail.com>
2022-01-30 22:15:20 +0300

    references

---
+++ 3/09-c-tables-with-references.sql
@@ -0,0 +1,20 @@
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    street VARCHAR(500) NOT NULL,
+    house_number VARCHAR(50),
+    city_id INT REFERENCES cities (id)
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT REFERENCES addresses (id) 
+);
+
