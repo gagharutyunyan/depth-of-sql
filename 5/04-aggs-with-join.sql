c8508495aa7cc01b42b3630effec9dcb6fbdeab5
gagharutyunyan <gagharut@gmail.com>
2022-02-06 20:00:06 +0300

    group by, where, join, aggregation fns

--- 5/04-aggs-with-join.sql
+++ 5/04-aggs-with-join.sql
@@ -1,4 +1,5 @@
-SELECT amount_billed, amount_tipped, p.name
+SELECT ROUND(AVG(amount_billed)) as amount_billed, MAX(amount_tipped) as amount_tipped, p.name
 FROM bookings 
 INNER JOIN payment_methods as p ON p.id = bookings.payment_id
-ORDER BY bookings.amount_billed DESC;
+WHERE p.name = 'Credit Card'
+GROUP BY p.name;
