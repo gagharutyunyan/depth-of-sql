27e37d6c108475c9adae14d5879ff324194af593
gagharutyunyan <gagharut@gmail.com>
2022-02-11 20:55:55 +0300

    TIMESTAMPDIFF

--- 6/04-date-time-difference.sql
+++ 6/04-date-time-difference.sql
@@ -1,3 +1,5 @@
 SELECT data1 - data2 FROM some_table; -- returns {"hours": 1, "minutes": 5, "seconds": 54} this is data type INTERVAL
 SELECT data1 - data2 FROM some_table; -- {"minutes": 5, "seconds": 54}
 SELECT data1 - data2 FROM some_table; -- {"seconds": 7}
+
+SELECT TIMESTAMPDIFF(MINUTE, data1, data2) FROM some_table; -- 254 it's like 4 hours
