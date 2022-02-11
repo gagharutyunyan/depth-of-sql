6e32ab12ac81a623c0bde502e5074ad98fcf599d
gagharutyunyan <gagharut@gmail.com>
2022-02-11 21:01:38 +0300

    date and timestamp calculation

--- 6/04-date-time-difference.sql
+++ 6/04-date-time-difference.sql
@@ -3,3 +3,12 @@ SELECT data1 - data2 FROM some_table; -- {"minutes": 5, "seconds": 54}
 SELECT data1 - data2 FROM some_table; -- {"seconds": 7}
 
 SELECT TIMESTAMPDIFF(MINUTE, data1, data2) FROM some_table; -- 254 it's like 4 hours
+
+-- works only with TIMESTAMP
+
+SELECT data1 - data2 FROM some_table; -- returns 253 days
+
+-- works only with DATE
+
+SELECT NOW()::DATE;
+SELECT NOW()::TIMESTAMP;
