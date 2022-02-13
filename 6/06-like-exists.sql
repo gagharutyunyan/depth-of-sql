4f5658cffc33c1441c5d965d51c3b38d5a431acf
gagharutyunyan <gagharut@gmail.com>
2022-02-13 20:31:17 +0300

    test costs of queries

--- 6/06-like-exists.sql
+++ 6/06-like-exists.sql
@@ -1,7 +1,7 @@
--- SELECT first_name like 'ar%' FROM users;
+-- explain SELECT first_name like 'ga%' FROM users;
+
+explain (FORMAT JSON) SELECT first_name FROM users
+WHERE id = 3028 and first_name = 'gag';
 
 -- SELECT first_name FROM users
--- WHERE first_name LIKE 'ar%';
-
-SELECT first_name FROM users
-WHERE first_name ILIKE 'mar%'; -- ilike vs like, like filtiring storngly, but ilike don't, he is not caring about uppercase and etc 
+-- WHERE first_name ILIKE 'mar%'; -- ilike vs like, like filtiring storngly, but ilike don't, he is not caring about uppercase and etc 
